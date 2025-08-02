b84d5d363e24936809a78f83fa0d434eeb34eae2
Hasham <m.hasham681@gmail.com>
2025-08-02 19:16:24 +0500

    feat: move implementation to a separate function

--- main.py
+++ main.py
@@ -1,24 +1,7 @@
-from services.computer_vision import computer_vision_client
-from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
-import time
-
 # First, we need an image that we want to process.
-image = "https://learn.microsoft.com/azure/ai-services/computer-vision/media/quickstarts/presentation.png"
+from services.computer_vision import get_text_from_file
 
-# Then, we need to call the computer vision client to process the image.
-response = computer_vision_client.read(image, raw=True)
-operation_id = response.headers["Operation-Location"].split("/")[-1]
 
-# Check again and again for the status of the operation, and if it's succeeded, we can get the result.
-while True:
-    result = computer_vision_client.get_read_result(operation_id)
-    if result.status == OperationStatusCodes.succeeded:
-        break
-    time.sleep(1)
-
-if result is None or result.status != OperationStatusCodes.succeeded:
-    raise Exception("Failed to read the image")
-
-for text_result in result.analyze_result.read_results:
-    for line in text_result.lines:
-        print(line.text)
+# image = "https://learn.microsoft.com/azure/ai-services/computer-vision/media/quickstarts/presentation.png"
+# results = get_text_from_file(image)
+# print(results)
--- services/computer_vision.py
+++ services/computer_vision.py
@@ -1,7 +1,10 @@
+from typing import Dict, List
 from dotenv import load_dotenv
 import os
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from msrest.authentication import CognitiveServicesCredentials
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+import time
 
 load_dotenv()
 
@@ -11,3 +14,25 @@ VISION_REGION = os.getenv("VISION_REGION")
 
 credentials = CognitiveServicesCredentials(VISION_KEY)
 computer_vision_client = ComputerVisionClient(VISION_ENDPOINT, credentials)
+
+def get_text_from_file(image: str) -> Dict[str, List[str]]:
+    # Then, we need to call the computer vision client to process the image.
+    response = computer_vision_client.read(image, raw=True)
+    operation_id = response.headers["Operation-Location"].split("/")[-1]
+
+    # Check again and again for the status of the operation, and if it's succeeded, we can get the result.
+    while True:
+        result = computer_vision_client.get_read_result(operation_id)
+        if result.status == OperationStatusCodes.succeeded:
+            break
+        time.sleep(1)
+
+    if result is None or result.status != OperationStatusCodes.succeeded:
+        raise Exception("Failed to read the image")
+
+    results: Dict[str, List[str]] = {}
+    for text_result in result.analyze_result.read_results:
+        lines: List[str] = [line.text for line in text_result.lines]
+        results[text_result.page] = lines
+
+    return results
